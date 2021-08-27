7ca11d75a0cebdeb32aff31c675a1ac498baaa20
Anandhan <anandhanedom@gmail.com>
2021-08-27 22:56:40 +0530

    feat: learn moustache, v-html, v-text and v-bind

---
+++ hello-world/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>{{ greet }} {{ name }}</div>
+    <div v-text="channel"></div>
+    <div v-html="channel"></div>
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">Bind</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "John Doe",
+      greet: "Hello",
+      channel: "<b>Codevolution</b>",
+      headingId: "heading",
+      isDisabled: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
