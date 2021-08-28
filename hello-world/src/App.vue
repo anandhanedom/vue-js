f56583acf333d4730bc25d048a3cb6977df14949
Anandhan <anandhanedom@gmail.com>
2021-08-28 10:40:27 +0530

    feat: binding classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <div>{{ greet }} {{ name }}</div>
+    <!-- <div>{{ greet }} {{ name }}</div>
     <div v-text="channel"></div>
-    <div v-html="channel"></div>
+    <div v-html="channel"></div> -->
+
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underlined text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
+    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2
+      v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']"
+    >
+      Array conditional movie
+    </h2>
+    <h2
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldOut,
+        'sold-out': isSoldOut,
+      }"
+    >
+      Object conditional movie
+    </h2>
   </div>
 </template>
 
@@ -13,11 +33,14 @@ export default {
   name: "App",
   data() {
     return {
-      name: "John Doe",
-      greet: "Hello",
-      channel: "<b>Codevolution</b>",
+      // name: "John Doe",
+      // greet: "Hello",
+      // channel: "<b>Codevolution</b>",
       headingId: "heading",
       isDisabled: false,
+      status: "danger",
+      isPromoted: false,
+      isSoldOut: false,
     };
   },
 };
@@ -31,5 +54,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.success,
+.new {
+  color: green;
+}
+
+.danger,
+.sold-out {
+  color: red;
+}
+
+.promoted {
+  font-style: italic;
 }
 </style>
