45ef929372542ba0c41e622bc69debb3fde163b0
Anandhan <anandhanedom@gmail.com>
2021-09-01 12:57:34 +0530

    feat: dynamic components and keep alive tag

---
+++ 07_dynamic_components/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="activeTab = 'TabA'">Tab A</button>
+    <button @click="activeTab = 'TabB'">Tab B</button>
+    <button @click="activeTab = 'TabC'">Tab C</button>
+
+    <!-- <TabA v-if="activeTab === 'TabA'" />
+    <TabB v-if="activeTab === 'TabB'" />
+    <TabC v-if="activeTab === 'TabC'" /> -->
+
+    <keep-alive>
+      <component :is="activeTab" />
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TabA from "./components/TabA.vue";
+import TabB from "./components/TabB.vue";
+import TabC from "./components/TabC.vue";
+
+export default {
+  name: "App",
+  components: {
+    TabA,
+    TabB,
+    TabC,
+  },
+  data() {
+    return {
+      activeTab: "TabA",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h4 {
+  color: orange;
+}
+</style>
