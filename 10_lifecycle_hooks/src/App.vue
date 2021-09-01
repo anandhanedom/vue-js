ae74430c7e0f85ba36069533b15547acb6c97319
Anandhan <anandhanedom@gmail.com>
2021-09-01 15:19:06 +0530

    feat: boilderplate for lifecycle hooks

---
+++ 10_lifecycle_hooks/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h4 {
+  color: orange;
+}
+</style>
