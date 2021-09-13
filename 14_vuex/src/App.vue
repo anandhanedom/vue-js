afa6a2e11cd5498b207db9a499a910ad0d1de00a
Anandhan <anandhanedom@gmail.com>
2021-09-13 15:33:04 +0530

    feat: setup basic project

--- 14_vuex/src/App.vue
+++ 14_vuex/src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <product-list-one :products="products"></product-list-one>
+    <product-list-two :products="products"></product-list-two>
   </div>
 </template>
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+
+export default {
+  name: "app",
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo,
+  },
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Shiny Star", price: 40 },
+        { name: "Green Shells", price: 60 },
+        { name: "Red Shells", price: 80 },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: Ubuntu;
+  color: #555;
 }
 </style>
