16b619e3e8ee7f21b56d3e9956874046bf12e591
Anandhan <anandhanedom@gmail.com>
2021-09-13 13:38:59 +0530

    feat: add params

--- 13_vue_router/src/router/index.js
+++ 13_vue_router/src/router/index.js
@@ -1,50 +1,50 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import Jobs from '../views/jobs/Jobs.vue';
-import JobDetails from '../views/jobs/JobDetails.vue';
-import NotFound from '../views/NotFound.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Jobs from "../views/jobs/Jobs.vue";
+import JobDetails from "../views/jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About,
   },
   {
-    path: '/jobs',
-    name: 'Jobs',
+    path: "/jobs",
+    name: "Jobs",
     component: Jobs,
   },
   {
-    path: '/jobs/:id',
-    name: 'JobDetails',
+    path: "/jobs/:id",
+    name: "JobDetails",
     component: JobDetails,
     props: true,
   },
   // redirection
   {
-    path: '/all-jobs',
-    redirect: '/jobs',
+    path: "/all-jobs",
+    redirect: "/jobs",
   },
   // catch all 404
   {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
+    path: "/:catchAll(.*)",
+    name: "NotFound",
     component: NotFound,
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
